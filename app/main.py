8bb96993f614ff40999c39cceb6587ecbb6c675a
Hilbert Danrlley <hilbertmiguel14@gmail.com>
2026-09-14 16:01:34 -0300

    migracoes com alembic adicionado

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-from app.database import engine, Base
 from app.routers import clinic, auth
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="MedSync API",
